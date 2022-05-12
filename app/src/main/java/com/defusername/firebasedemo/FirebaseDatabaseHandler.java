0768f5f52bd7abebc099d94fbc8308d1896ab32d
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-05-13 04:23:55 +0600

    Setup primary firebase database

---
+++ app/src/main/java/com/defusername/firebasedemo/FirebaseDatabaseHandler.java
@@ -0,0 +1,7 @@
+package com.defusername.firebasedemo;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class FirebaseDatabaseHandler {
+    public static final FirebaseDatabase FIREBASE_DATABASE = FirebaseDatabase.getInstance();
+}
--- app/src/main/java/com/defusername/firebasedemo/MainActivity.java
+++ app/src/main/java/com/defusername/firebasedemo/MainActivity.java
@@ -1,14 +1,60 @@
 package com.defusername.firebasedemo;
 
+import static com.defusername.firebasedemo.FirebaseDatabaseHandler.FIREBASE_DATABASE;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonWriteData;
+    private TextView textViewDisplayData;
+    private final DatabaseReference mRef = FIREBASE_DATABASE.getReference("message");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+        handleButtonClicks();
+        handleDatabaseRefChanges();
+    }
+
+    private void initViews() {
+        buttonWriteData = findViewById(R.id.Button_WriteData);
+        textViewDisplayData = findViewById(R.id.TextView_DisplayData);
+    }
+
+    private void handleButtonClicks() {
+        buttonWriteData.setOnClickListener((view) -> {
+            mRef.setValue("Hello darkness my old friend.");
+        });
+    }
+
+    private void handleDatabaseRefChanges() {
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String value = snapshot.getValue(String.class);
+                textViewDisplayData.setText(value);
+            }
+
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                textViewDisplayData.setText("Error " + error);
+            }
+        });
     }
 }
