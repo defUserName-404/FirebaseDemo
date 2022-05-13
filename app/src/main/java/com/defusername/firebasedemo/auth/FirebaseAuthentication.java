f9748f65ed0858d4222e86cf13845579d652e9f0
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-05-14 03:22:36 +0600

    App launches new activity after successful sign in now

--- app/src/main/java/com/defusername/firebasedemo/auth/FirebaseAuthentication.java
+++ app/src/main/java/com/defusername/firebasedemo/auth/FirebaseAuthentication.java
@@ -1,14 +1,25 @@
 package com.defusername.firebasedemo.auth;
 
 import static com.defusername.firebasedemo.FirebaseHandler.AUTH_UI;
-import static com.defusername.firebasedemo.FirebaseHandler.FIREBASE_AUTH;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.MainThread;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.defusername.firebasedemo.MainActivity;
 import com.defusername.firebasedemo.R;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class FirebaseAuthentication extends AppCompatActivity {
 
@@ -20,16 +31,62 @@ public class FirebaseAuthentication extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firebase_authentication);
 
-        buttonSignIn = findViewById(R.id.Button_SignIn);
-        if (FIREBASE_AUTH.getCurrentUser() != null) {
-            finish();
-        }
+        initViews();
+        handleButtonClicks();
+    }
 
+    private void initViews() {
+        buttonSignIn = findViewById(R.id.Button_SignIn);
+    }
+
+    private void handleButtonClicks() {
         buttonSignIn.setOnClickListener((view -> {
             startActivityForResult(AUTH_UI
                             .createSignInIntentBuilder()
                             .build(),
                     RC_SIGN_IN);
         }));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            handleSignInResponse(resultCode, data);
+        }
+    }
+
+    @MainThread
+    private void handleSignInResponse(int resultCode, Intent data) {
+        IdpResponse response = IdpResponse.fromResultIntent(data);
+        Toast toast;
+
+        if (resultCode == RESULT_OK) {
+            startActivity(createIntent(this));
+            finish();
+        } else {
+            if (response == null) {
+                toast = Toast.makeText(this, "Sign in was cancelled!", Toast.LENGTH_LONG);
+                toast.show();
+                return;
+            }
+            if (Objects.requireNonNull(response.getError()).getErrorCode() == ErrorCodes.NO_NETWORK) {
+                toast = Toast.makeText(this, "You have no internet connection", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        }
+
+        toast = Toast.makeText(this, "Unknown Error!", Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    @SuppressLint("RestrictedApi")
+    public static Intent createIntent(Context context) {
+        Intent intent = new Intent(context, SignInActivity.class);
+        intent.putExtra("USER_NAME", AUTH_UI.getAuth().getCurrentUser().getDisplayName());
+        intent.putExtra("EMAIL", AUTH_UI.getAuth().getCurrentUser().getEmail());
+
+        return intent;
     }
 }
---
+++ app/src/main/java/com/defusername/firebasedemo/auth/SignInActivity.java
@@ -0,0 +1,38 @@
+package com.defusername.firebasedemo.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.defusername.firebasedemo.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    private TextView textViewUserName, textViewEmail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        initViews();
+        String username = getIntent().getStringExtra("USER_NAME");
+        String email = getIntent().getStringExtra("EMAIL");
+        showData(username, email);
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void showData(String username, String email) {
+        if (username != null)
+            textViewUserName.setText("Hello " + username);
+        if (email != null)
+            textViewEmail.setText("Your email is " + email);
+    }
+
+    private void initViews() {
+        textViewUserName = findViewById(R.id.TextView_UserName);
+        textViewEmail = findViewById(R.id.TextView_Email);
+    }
+}
