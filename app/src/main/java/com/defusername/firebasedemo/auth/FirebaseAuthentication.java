c596ce42054c21ff36322b4d829621470a85d989
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-05-15 20:18:07 +0600

    Added sign in credentials with SHA256

--- app/src/main/java/com/defusername/firebasedemo/auth/FirebaseAuthentication.java
+++ app/src/main/java/com/defusername/firebasedemo/auth/FirebaseAuthentication.java
@@ -1,26 +1,28 @@
 package com.defusername.firebasedemo.auth;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.MainThread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.defusername.firebasedemo.R;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
 import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Objects;
 
 public class FirebaseAuthentication extends AppCompatActivity {
 
-	private static final int RC_SIGN_IN = 100;
 	private Button buttonSignIn;
 
 	@Override
@@ -37,33 +39,33 @@ public class FirebaseAuthentication extends AppCompatActivity {
 	}
 
 	private void handleButtonClicks() {
-		buttonSignIn.setOnClickListener((view -> {
-			startActivityForResult(AuthUI.getInstance()
-							.createSignInIntentBuilder()
-							.setAvailableProviders(Collections.singletonList(
-									new AuthUI.IdpConfig.EmailBuilder().build()))
-							.build(),
-					RC_SIGN_IN);
-		}));
+		buttonSignIn.setOnClickListener((view -> createSignInIntent()));
 	}
 
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-
-		if (requestCode == RC_SIGN_IN) {
-			handleSignInResponse(resultCode, data);
-		}
-	}
+	private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+			new FirebaseAuthUIActivityResultContract(),
+			this::onSignInResult
+	);
 
 	@MainThread
-	private void handleSignInResponse(int resultCode, Intent data) {
-		IdpResponse response = IdpResponse.fromResultIntent(data);
+	private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+		IdpResponse response = result.getIdpResponse();
 		Toast toast;
 
-		if (resultCode == RESULT_OK) {
-			startActivity(createIntent(this));
+		if (result.getResultCode() == RESULT_OK) {
+			Intent signInIntent = new Intent(this, SignInActivity.class);
+
+			FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+			assert user != null;
+			signInIntent.putExtra("USER_NAME", user.getDisplayName());
+			signInIntent.putExtra("EMAIL", user.getEmail());
+
+			startActivity(signInIntent);
+
 			finish();
+			
+			return;
 		} else {
 			if (response == null) {
 				toast = Toast.makeText(this, "Sign in was cancelled!", Toast.LENGTH_LONG);
@@ -80,18 +82,15 @@ public class FirebaseAuthentication extends AppCompatActivity {
 		toast.show();
 	}
 
-	@SuppressLint("RestrictedApi")
-	public static Intent createIntent(Context context) {
-		Intent intent = new Intent(context, SignInActivity.class);
-		intent.putExtra("USER_NAME", Objects.requireNonNull(AuthUI.getInstance()
-				.getAuth()
-				.getCurrentUser())
-				.getDisplayName());
-		intent.putExtra("EMAIL", Objects.requireNonNull(AuthUI.getInstance()
-				.getAuth()
-				.getCurrentUser())
-				.getEmail());
+	public void createSignInIntent() {
+		Intent signInIntent = AuthUI.getInstance()
+				.createSignInIntentBuilder()
+				.setAvailableProviders(Arrays.asList(
+						new AuthUI.IdpConfig.EmailBuilder().build(),
+						new AuthUI.IdpConfig.GoogleBuilder().build())
+				)
+				.build();
 
-		return intent;
+		signInLauncher.launch(signInIntent);
 	}
 }
