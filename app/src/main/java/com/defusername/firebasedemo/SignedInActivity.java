120d6c34490d95253587ebb7b1275a7d99a9a518
Moshiur Rahman Nafi <moshiur.r.nafi@gmail.com>
2022-05-20 00:34:57 +0600

    Firebase storage now stores pictures

--- app/src/main/java/com/defusername/firebasedemo/SignedInActivity.java
+++ app/src/main/java/com/defusername/firebasedemo/SignedInActivity.java
@@ -5,22 +5,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.defusername.firebasedemo.auth.FirebaseAuthenticationActivity;
 import com.defusername.firebasedemo.auth.User;
 import com.defusername.firebasedemo.realtime_database.RealtimeDatabase;
+import com.defusername.firebasedemo.storage.Storage;
 import com.firebase.ui.auth.AuthUI;
 
 public class SignedInActivity extends AppCompatActivity {
 
 	private TextView textViewUserName, textViewEmail;
-	private Button buttonSignOut, buttonWriteToDatabase;
+	private Button buttonSignOut, buttonWriteToDatabase, buttonSetProfilePicture;
+	private ImageView imageViewProfilePicture;
 	private String username, email;
 	private RealtimeDatabase firebaseRealtimeDatabase;
+	private static final int SELECT_IMAGE_CODE = 2;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,24 +38,32 @@ public class SignedInActivity extends AppCompatActivity {
 	}
 
 	private void handleButtonClicks() {
-		buttonSignOut.setOnClickListener(view -> {
-			AuthUI.getInstance()
-					.signOut(this)
-					.addOnCompleteListener(task -> {
-						if (task.isSuccessful()) {
-							startActivity(new Intent(SignedInActivity.this, FirebaseAuthenticationActivity.class));
-							finish();
-						} else
-							Log.d("SIGNED_IN_ACTIVITY", "Can't log out");
-					});
-		});
+		buttonSignOut.setOnClickListener(view -> AuthUI.getInstance()
+				.signOut(this)
+				.addOnCompleteListener(task -> {
+					if (task.isSuccessful()) {
+						startActivity(new Intent(SignedInActivity.this,
+								FirebaseAuthenticationActivity.class));
+						finish();
+					} else
+						Log.d("SIGNED_IN_ACTIVITY", "Can't log out");
+				}));
 
 		buttonWriteToDatabase.setOnClickListener(view -> {
 					User user = new User(username, email);
 					if (firebaseRealtimeDatabase.writeToDatabase(user))
-						Toast.makeText(this, "Write to database successful", Toast.LENGTH_SHORT).show();
+						Toast.makeText(this, "Write to database successful",
+								Toast.LENGTH_SHORT).
+								show();
 				}
 		);
+
+		buttonSetProfilePicture.setOnClickListener(view -> {
+			Intent intent = new Intent();
+			intent.setType("image/*");
+			intent.setAction(Intent.ACTION_GET_CONTENT);
+			startActivityForResult(Intent.createChooser(intent, "Title"), SELECT_IMAGE_CODE);
+		});
 	}
 
 	@SuppressLint("SetTextI18n")
@@ -61,11 +74,24 @@ public class SignedInActivity extends AppCompatActivity {
 			textViewEmail.setText("Email: " + email);
 	}
 
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		if (requestCode == SELECT_IMAGE_CODE) {
+			assert data != null;
+			Storage.getInstance()
+					.uploadPicture(data.getData(), username);
+		}
+	}
+
 	private void initViews() {
 		textViewUserName = findViewById(R.id.TextView_UserName);
 		textViewEmail = findViewById(R.id.TextView_Email);
 		buttonSignOut = findViewById(R.id.Button_SignOut);
 		buttonWriteToDatabase = findViewById(R.id.Button_WriteToDatabase);
+		buttonSetProfilePicture = findViewById(R.id.Button_SetProfilePicture);
+		imageViewProfilePicture = findViewById(R.id.ImageView_ProfilePicture);
 
 		username = getIntent().getStringExtra("USERNAME");
 		email = getIntent().getStringExtra("EMAIL");
--- app/src/main/java/com/defusername/firebasedemo/storage/Storage.java
+++ app/src/main/java/com/defusername/firebasedemo/storage/Storage.java
@@ -1,4 +1,40 @@
 package com.defusername.firebasedemo.storage;
 
+import android.net.Uri;
+import android.util.Log;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
 public class Storage {
+
+	private static Storage instance;
+	private final StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+
+	private Storage() {
+	}
+
+	public static Storage getInstance() {
+		if (instance == null)
+			instance = new Storage();
+
+		return instance;
+	}
+
+	public void uploadPicture(Uri imageUri, String fileName) {
+		StorageReference imageRef = storageRef.child("images/" + fileName + ".jpg");
+
+		imageRef.putFile(imageUri)
+				.addOnFailureListener(exception -> {
+					// Handle unsuccessful uploads)
+					// Register observers to listen for when the download is done or if it fails
+					Log.e("Message", "Upload failed");
+				})
+				.addOnSuccessListener(taskSnapshot -> {
+					// taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+					// ...
+					Log.i("Message", "Upload successful");
+					Log.i("Metadata", taskSnapshot.getMetadata().toString());
+				});
+	}
 }
